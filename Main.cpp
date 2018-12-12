0502d39805456b703f259247bd2e3b86f34dfe27
linzihao <104664078@163.com>
2018-12-12 15:12:01 +0800

    change some name

--- Main.cpp
+++ Main.cpp
@@ -5,10 +5,10 @@
 int main(int argc, char **argv) {
     Screen screen(argc, argv);
     screen.Init();
-    for (int i = 0; i < WINDOWS_SIZE_X; i += Screen::blockx) {
-        for (int j = 0; j < WINDOWS_SIZE_Y; j += Screen::blocky) {
+    for (int i = 0; i < WINDOWS_SIZE_X; i += Screen::blockSize_x) {
+        for (int j = 0; j < WINDOWS_SIZE_Y; j += Screen::blockSize_y) {
             for (int k = 0; k < 3; k++)
-                Screen::color[i][j][k] = 1;
+                Screen::blockColor[i][j][k] = 1;
         }
     }
     screen.Run();
